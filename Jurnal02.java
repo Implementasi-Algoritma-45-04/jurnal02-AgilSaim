02f0a58d924818d71364b72caa76d0326a8ecfd1
Agil Saim Setiawan <agilsaimsetiawan23@gmail.com>
2021-10-01 14:07:03 +0700

    Solusi Jurnal 02

---
+++ Jurnal02.java
@@ -0,0 +1,7 @@
+public class Jurnal02 {
+	public static void main(String[] args) {
+		System.out.println("NIM  : 6706213047");
+		System.out.println("Nama : Agil Saim Setiawan");
+		System.out.println("Kelas: D3IF-45-04");
+	}
+}
